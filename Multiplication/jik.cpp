0b38375400d6b3b5e8458743ef9d57aa89c25603
Anup-Nayak <gpnayak1930@gmail.com>
2024-04-24 17:51:36 +0530

    mult and transpose data and code

---
+++ Multiplication/jik.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <fstream>
+#include <ctime>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+int generateRandomNumber() {
+    return rand() % 10;
+}
+
+void initializeMatrix(vector<vector<int>>& matrix, int n) {
+    matrix.resize(n, vector<int>(n));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            matrix[i][j] = generateRandomNumber();
+        }
+    }
+}
+
+void printMatrix(const vector<vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+
+vector<vector<int>> matrixProduct(const vector<vector<int>>& A, const vector<vector<int>>& B) {
+    int n = A.size();
+    vector<vector<int>> result(n, vector<int>(n, 0));
+
+    for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < n; ++i) {
+            for (int k = 0; k < n; ++k) {
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    ofstream outFile("jik.csv");
+    srand(time(nullptr));
+
+    int N = 6002;
+
+    for (int n = 1; n <= N; n=n+50) {
+        
+        vector<vector<int>> A, B;
+        initializeMatrix(A, n);
+        initializeMatrix(B, n);
+
+        const int numTrials = 1;
+        double totalTime = 0.0;
+
+        for (int i = 0; i < numTrials; ++i) {
+            auto start = high_resolution_clock::now();
+
+            vector<vector<int>> product = matrixProduct(A, B);
+
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
+            totalTime += duration.count();
+        }
+
+        double averageTime = totalTime / numTrials;
+        outFile << n << "," << averageTime/1000000 << endl;
+        cout << "For n = " << n << ", jik : " << averageTime/1000000 << " seconds" << endl;
+    }
+
+    return 0;
+}
---
+++ Transpose/ji.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <fstream>
+#include <ctime>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+int generateRandomNumber() {
+    return rand() % 10;
+}
+
+void initializeMatrix(vector<vector<int>>& matrix, int n) {
+    matrix.resize(n, vector<int>(n));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            matrix[i][j] = generateRandomNumber();
+        }
+    }
+}
+
+void printMatrix(const vector<vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+
+void transposeInPlace(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    for (int j = 0; j < n; ++j) {
+        for (int i = j + 1; i < n; ++i) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+}
+
+vector<vector<int>> transposeWithAdditionalArray(const vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    vector<vector<int>> transposed(n, vector<int>(n));
+    for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < n; ++i) {
+            transposed[j][i] = matrix[i][j];
+        }
+    }
+    return transposed;
+}
+
+int main() {
+    srand(time(nullptr));
+    ofstream outFile("inPlace(ji).csv");
+    ofstream outFile1("array(ji).csv");
+    const int maxSize = 10002;
+
+    for (int n = 1; n <= maxSize; n = n+50) {
+        vector<vector<int>> matrix;
+        initializeMatrix(matrix, n);
+
+        const int numTrials = 3;
+        double totalTime1 = 0.0;
+        double totalTime2 = 0.0;
+
+        for (int i = 0; i < numTrials; ++i) {
+
+            auto start = high_resolution_clock::now();
+
+            transposeInPlace(matrix);
+
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
+            double ans = duration.count() / 1000;
+            totalTime1+= ans;
+            
+
+
+
+
+            start = high_resolution_clock::now();
+
+            vector<vector<int>> transposed = transposeWithAdditionalArray(matrix);
+
+            stop = high_resolution_clock::now();
+            duration = duration_cast<microseconds>(stop - start);
+            double ans1 = duration.count() / 1000;
+            totalTime2+= ans1;
+            
+        }
+        outFile << n << "," << totalTime1/numTrials << endl;
+        cout << n << "," << totalTime1/numTrials << endl;
+
+        outFile1 << n << "," << totalTime2/numTrials << endl;
+        cout << n << "," << totalTime2/numTrials << endl;
+    }
+
+    
+
+    return 0;
+}
